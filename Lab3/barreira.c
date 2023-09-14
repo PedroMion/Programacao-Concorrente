b200f07cbbac8d515d9ebf5fde8b678726608afe
Pedro Mion <pedromionbraga@gmail.com>
2023-09-14 19:33:39 -0300

    Código do lab3

---
+++ Lab3/barreira.c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+
+int N_THREADS;
+int aux;
+pthread_mutex_t mutex;
+pthread_cond_t x_cond;
+
+typedef struct{
+ int idThread;
+} tArgs;
+
+void barreira(int idThread) {
+  pthread_mutex_lock(&mutex);
+  aux += 1;
+  if(aux != N_THREADS) {
+    pthread_cond_wait(&x_cond, &mutex);
+  }
+  else {
+    pthread_cond_broadcast(&x_cond);
+    aux = 0;
+  }
+
+ pthread_mutex_unlock(&mutex);
+}
+
+void * tarefa(void *arg) {
+ tArgs *args = (tArgs*) arg;
+
+ printf("Olá da thread %d\n", args->idThread);
+ barreira(args->idThread);
+
+ printf("Que dia bonito %d\n", args->idThread);
+ barreira(args->idThread);
+
+ printf("Até breve da thread %d\n", args->idThread);
+ barreira(args->idThread);
+
+ pthread_exit(NULL);
+}
+
+
+int main(int argc, char* argv[]) {
+ pthread_t *tid;
+ tArgs *args;
+
+ if(argc<2) {
+    printf("Digite: %s <numero de threads>\n", argv[0]);
+    return 1;
+ }
+
+ N_THREADS = atoi(argv[1]);
+
+ tid = (pthread_t*) malloc(sizeof(pthread_t)*N_THREADS);
+ if(tid==NULL) {puts("ERRO--malloc"); return 2;}
+
+ args = (tArgs*) malloc(sizeof(tArgs)*N_THREADS);
+ if(args==NULL) {puts("ERRO--malloc"); return 2;}
+
+ pthread_mutex_init(&mutex, NULL);
+ pthread_cond_init(&x_cond, NULL);
+
+ aux = 0;
+ for(int i=0; i<N_THREADS; i += 1) {
+    (args+i)->idThread = i;
+
+    if(pthread_create(tid+i, NULL, tarefa, (void*) (args+i))){
+       puts("ERRO--pthread_create"); return 3;
+    }
+ }
+
+ for(int i=0; i<N_THREADS; i++) {
+    pthread_join(*(tid+i), NULL);
+ }
+
+ pthread_mutex_destroy(&mutex);
+
+ free(args);
+ free(tid);
+
+ return 0;
+}
